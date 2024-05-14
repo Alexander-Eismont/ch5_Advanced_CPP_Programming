1060cb0e852b59f4bb3bb642dca15d76ffe91ee0
Aleksander Eismont <alex.v.eismont@gmail.com>
2024-05-14 11:48:21 +0300

    updated solution and implementation of template functions

--- lesson-04/1_Squaring/main.cpp
+++ lesson-04/1_Squaring/main.cpp
@@ -2,21 +2,29 @@
 #include <vector>
 
 template<typename T>
-T square(T num);
+T square(T x);
 
 template<typename T>
-void square(T &data, unsigned int size);
+std::vector<T> square(const std::vector<T>& data);
+
+template<typename T>
+void printVec(std::vector<T> vec);
 
 int main()
 {
     int num = 4;
+    std::cout << "Num = " << num << "\n";
+
+    int squared_num = square(num);
+    std::cout << "Squared num = " << squared_num << "\n";
+
     std::vector<int> nums = {-1, 4, 8};
+    std::cout << "Nums = ";
+    printVec(nums);
 
-    std::cout << square(num) << "\n";
-
-    square(nums, nums.size());
-    for (auto item: nums)
-        std::cout << item << " ";
+    std::vector<int> squared_nums = square(nums);
+    std::cout << "Squared nums = ";
+    printVec(squared_nums);
 
     std::cout << "\n";
 
@@ -24,11 +32,25 @@ int main()
 }
 
 template<typename T>
-T square(T num) { return num * num; }
+T square(T x) { return x * x; }
 
 template<typename T>
-void square(T &data, unsigned int size)
+std::vector<T> square(const std::vector<T>& data)
 {
-    for (int i = 0; i < size; i++)
-        data[i] *= data[i];
+    std::vector<T> result;
+    result.reserve(data.size());
+
+    for (const auto& elem: data)
+        result.push_back(elem * elem);
+
+    return result;
+}
+
+template<typename T>
+void printVec(std::vector<T> vec)
+{
+    for (const auto& elem: vec)
+        std::cout << elem << " ";
+
+    std::cout << "\n";
 }
