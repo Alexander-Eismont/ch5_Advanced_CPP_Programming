359648ac4c9d91984fda2479a44d2d9c6ddd0f85
Aleksander Eismont <alex.v.eismont@gmail.com>
2024-05-14 13:43:47 +0300

    implementation of pure operator () has been made

--- lesson-04/3_Number_Counter/main.cpp
+++ lesson-04/3_Number_Counter/main.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+class Functor
+{
+public:
+    Functor() : m_sum(0), m_count(0), m_denominator(3) {}
+
+    void operator()(int num)
+    {
+        if (num % m_denominator == 0)
+        {
+            m_sum += num;
+            m_count++;
+        }
+    }
+
+    int get_sum() const { return m_sum; }
+    int get_count() const { return m_count; }
+
+private:
+    int m_sum;
+    int m_count;
+    int m_denominator;
+};
 
 int main()
 {
     std::vector<int> nums = {4, 1, 3, 6, 25, 54};
-    int sum = 0;
-    int counter = 0;
-    int denominator = 3;
 
-    auto func = [&]()
-    {
-        if (denominator == 0)
-            throw std::runtime_error("Division by zero!");
+    Functor functor = std::for_each(nums.begin(), nums.end(), Functor());
 
-        for (const auto& num: nums)
-            if (num % denominator == 0)
-            {
-                counter++;
-                sum += num;
-            }
-
-        std::cout << "Sum = " << sum << "\n";
-        std::cout << "Count = " << counter << "\n";
-    };
-
-    try
-    {
-        func();
-    }
-    catch (std::runtime_error& err)
-    {
-        std::cerr << "Error! " << err.what() << "\n";
-    }
+    std::cout << "Sum = " << functor.get_sum() << "\n";
+    std::cout << "Count = " << functor.get_count() << "\n";
 
     return 0;
 }
